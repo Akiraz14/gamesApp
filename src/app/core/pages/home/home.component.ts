aa6693be57a1e9f963fc73cb2901615491d15b89
Akiraz <juanzap14@hotmail.com>
2024-02-12 14:08:17 -0500

    Home page

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+//third-party
+import { TranslocoModule } from '@ngneat/transloco';
 
 import { FooterComponent } from '@core/components/footer/footer.component';
 import { HeaderComponent } from '@core/components/header/header.component';
@@ -7,8 +11,11 @@ import { HeaderComponent } from '@core/components/header/header.component';
   selector: 'app-home',
   standalone: true,
   imports: [
+    RouterLink,
     FooterComponent,
     HeaderComponent,
+    //third-party
+    TranslocoModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
