afac154e66e85402b34a39feafe8f41961edbe69
Akiraz <juanzap14@hotmail.com>
2024-02-12 13:11:11 -0500

    About page

--- src/app/core/pages/about/about.component.ts
+++ src/app/core/pages/about/about.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//third-party
+import { TranslocoModule } from '@ngneat/transloco';
+
 import { FooterComponent } from '@core/components/footer/footer.component';
 import { HeaderComponent } from '@core/components/header/header.component';
 
@@ -9,6 +12,8 @@ import { HeaderComponent } from '@core/components/header/header.component';
   imports: [
     FooterComponent,
     HeaderComponent,
+    //third-party
+    TranslocoModule,
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
